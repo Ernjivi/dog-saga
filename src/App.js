a2044f3cb9681ffc549730c6088b71d2827f32de
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2018-11-23 19:38:45 -0600

    dog sagas

--- src/App.js
+++ src/App.js
@@ -2,27 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+
 class App extends Component {
   render() {
+    const {dog, fetching, onRequestDog, error} = this.props
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={dog || logo} className="App-logo" alt="logo" />
+          <h1>Dog saga!</h1>
+          {dog ? (
+            <p>Keep cliking to get a new dog</p>
+            ) : (
+            <p>Click to get a dog</p>
+          )}
+          {fetching ? (
+            <button disabled>Fetching...</button>
+          ): (
+            <button onClick={onRequestDog}>Request a dog</button>
+          )}
+          {error && <p style={{color: 'red'}}>Ups, error!</p>}
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({type: 'API_CALL_REQUEST'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import reducer from './redux';
+import { watcherSaga } from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// http://extension.remotedev.io/#usage
+const store = createStore(reducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools));
+
+sagaMiddleware.run(watcherSaga);
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
